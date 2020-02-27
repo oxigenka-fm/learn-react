a97a656edd1c90aa7f757a360fe86c3752a3503a
Alex Shu <axlwork@gmail.com>
2020-02-27 07:09:54 +0200

    Added UIKIT, HTML files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import "./uikit.min.css";
+// import "./uikit.min.js";
+// import "./uikit-icons.min.js"
+import "uikit";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const main = document.getElementsByTagName('main');
+ReactDOM.render(<App />, main[0]);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
